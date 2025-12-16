9a8d3b8ffbc1609615b930acb29ec96c51b2b820
apham <admin@houze.dns.army>
2025-12-16 01:26:36 +0100

    oair

---
+++ vars.oair.nix
@@ -0,0 +1,12 @@
+{
+  hostname= "oair";
+  targetUserName = "apham";
+  targetUserEmail = "apham@localhost";
+  keyboardLayout = "fr";
+  keyboardModel = "macbook79"; # for macbook use "macbook79"
+  wildcardDomain = "houze.dns.army";
+  enableKubernetes = true;
+  automaticlogin = true;
+  disableTurboBoost = true; # disable turbo boost for laptops and minipcs that run intel
+  # end of change this
+}
