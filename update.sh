ecbc435701db51e291190580bcb94dff97d7b69a
alainpham <13981031+alainpham@users.noreply.github.com>
2025-12-24 10:42:53 +0100

    if hw exists

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 sudo cp configuration.nix /etc/nixos
 sudo cp vars.${HOSTNAME}.nix /etc/nixos/vars.nix
+
+if [ -f "hw.${HOSTNAME}.nix" ]; then
 sudo cp hw.${HOSTNAME}.nix /etc/nixos/hw.nix
+fi
+
 sudo nixos-rebuild switch
