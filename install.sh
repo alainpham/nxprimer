dfb5df19cec8c59ebbe7445e1a5ac2cb7736e5c8
alainpham <13981031+alainpham@users.noreply.github.com>
2025-12-26 20:17:32 +0100

    swap

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ export TARGET_USERNAME=${1:-apham}
 lsblk
 
 read -p "Enter target disk (e.g., /dev/sda): " TARGETDISK
+read -p "Enter swap size in GiB (0 = no swap): " SWAP_GIB
 
 echo "Available configuration files:"
 
@@ -18,9 +19,7 @@ select file in vars.*.nix; do
     fi
 done
 
-options=("Skip selection" hw.*.nix)
-
-select file in "${options[@]}"; do
+select file in hw.*.nix; do
     if [[ -z $file ]]; then
         echo "No hardware selected"
         unset TARGETHW
@@ -32,31 +31,59 @@ select file in "${options[@]}"; do
     fi
 done
 
-parted -s ${TARGETDISK} mklabel gpt
+
+
+echo "Partitioning disk..."
+parted -s "${TARGETDISK}" mklabel gpt
 sleep 1
 
-parted -s ${TARGETDISK} mkpart primary fat32 1MiB 1GiB
+echo "Creating EFI parition..."
+parted -s "${TARGETDISK}" mkpart primary fat32 1MiB 1GiB
+parted -s "${TARGETDISK}" set 1 esp on
 sleep 1
 
-parted -s ${TARGETDISK} set 1 esp on
+PART_START_ROOT="1GiB"
+
+if [[ "$SWAP_GIB" -gt 0 ]]; then
+    SWAP_END="$((1 + SWAP_GIB))GiB"
+
+    echo "Creating ${SWAP_GIB}GiB swap partition..."
+    parted -s "${TARGETDISK}" mkpart primary linux-swap 1GiB "${SWAP_END}"
+    sleep 1
+
+    PART_START_ROOT="${SWAP_END}"
+    ROOT_PART=3
+else
+    echo "No swap partition will be created."
+    ROOT_PART=2
+fi
+
+
+# Root
+echo "Creating Root parition..."
+parted -s "${TARGETDISK}" mkpart primary ext4 "${PART_START_ROOT}" 100%
 sleep 1
 
-parted -s ${TARGETDISK} mkpart primary ext4 1GiB 100%
-sleep 1
-
+# Formating
+echo "Formatting filesystems..."
 mkfs.fat -F 32 ${TARGETDISK}1
-sleep 1
-
 fatlabel ${TARGETDISK}1 NIXBOOT
 sleep 1
 
-if blkid "${TARGETDISK}2" >/dev/null 2>&1; then
-  mkfs.ext4 -F "${TARGETDISK}2" -L NIXROOT
-else
-  mkfs.ext4 "${TARGETDISK}2" -L NIXROOT
+if [[ "$SWAP_GIB" -gt 0 ]]; then
+    mkswap "${TARGETDISK}2"
+    swapon "${TARGETDISK}2"
 fi
 sleep 1
 
+if blkid "${TARGETDISK}${ROOT_PART}" >/dev/null 2>&1; then
+    mkfs.ext4 -F "${TARGETDISK}${ROOT_PART}" -L NIXROOT
+else
+    mkfs.ext4 "${TARGETDISK}${ROOT_PART}" -L NIXROOT
+fi
+sleep 1
+
+echo "Mounting filesystems..."
 mount /dev/disk/by-label/NIXROOT /mnt
 mkdir -p /mnt/boot
 mount -o umask=077 /dev/disk/by-label/NIXBOOT /mnt/boot
@@ -66,11 +93,7 @@ nixos-generate-config --root /mnt
 
 cp configuration.nix /mnt/etc/nixos/configuration.nix
 cp $TARGETVARS /mnt/etc/nixos/vars.nix
-
-if [ -f "$TARGETHW" ]; then
-  cp $TARGETHW /mnt/etc/nixos/hw.nix
-fi
-
+cp $TARGETHW /mnt/etc/nixos/hw.nix
 
 nixos-install --no-root-passwd
 
---
+++ vars.gnrc.nix
@@ -0,0 +1,13 @@
+{
+  hostname= "gnrc";
+  targetUserName = "apham";
+  targetUserEmail = "apham@localhost";
+  keyboardLayout = "fr";
+  keyboardModel = "pc105";
+  keyboardVariant = "";
+  wildcardDomain = "houze.dns.army";
+  enableKubernetes = true;
+  enableVirtualization = true;
+  automaticlogin = true;
+  disableTurboBoost = false; # disable turbo boost for laptops and minipcs that run intel where you want to avoid overheating
+}
