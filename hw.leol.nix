24f0e7b2a1cde98955c5187c5657e1454cdbbdba
alainpham <13981031+alainpham@users.noreply.github.com>
2025-12-29 18:26:37 +0100

    fix

--- hw.leol.nix
+++ hw.leol.nix
@@ -5,6 +5,7 @@
   boot.blacklistedKernelModules = [ ];
   
   hardware.nvidia = {
+    modesetting.enable = false;
     open = false;
     powerManagement.enable = false;
     prime = {
--- hw.lg15.nix
+++ hw.lg15.nix
@@ -3,34 +3,23 @@
   boot.extraModulePackages = [ ];
   boot.initrd.kernelModules = [ ];
   boot.blacklistedKernelModules = [ ];
-
+  
   hardware.nvidia = {
+    modesetting.enable = false;
     open = false;
     powerManagement.enable = false;
     prime = {
-      offload.enable = true;
+      offload.enable = false;
       offload.enableOffloadCmd = config.hardware.nvidia.prime.offload.enable;
       sync.enable = false;
-      nvidiaBusId = "PCI:01:00:0";
       intelBusId = "PCI:00:02:0";
+      nvidiaBusId = "PCI:01:00:0";
     };
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
   };
   services.xserver.videoDrivers = [
-    "modesetting"
     "nvidia"
-  ];  
-  services.xserver.deviceSection = ''
-    Driver "nvidia"
-    Option "AllowEmptyInitialConfiguration"
-  '';
-  services.xserver = {
-    enable = true;
-    xkb.layout = vars.keyboardLayout;
-    xkb.model = vars.keyboardModel;
-    xkb.variant = vars.keyboardVariant;
-    displayManager.startx.enable = true;
-  };
+  ];
   nixpkgs.config.nvidia.acceptLicense = true;
 
   hardware.graphics = {
