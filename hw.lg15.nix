8471f4696324a1ee2300c1f8d3faf58283249175
alainpham <13981031+alainpham@users.noreply.github.com>
2025-12-29 17:47:20 +0100

    hw

--- hw.lg15.nix
+++ hw.lg15.nix
@@ -17,8 +17,8 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
   services.xserver.videoDrivers = [
-    "modesetting"
     "nvidia"
+    "modesetting"
   ];  
   nixpkgs.config.nvidia.acceptLicense = true;
 
