c5a5cdb81270f0bd219b938af385f182d9353bee
alainpham <13981031+alainpham@users.noreply.github.com>
2025-12-29 17:53:49 +0100

    try fix

--- hw.lg15.nix
+++ hw.lg15.nix
@@ -5,15 +5,14 @@
   boot.blacklistedKernelModules = [ ];
 
   hardware.nvidia = {
-    modesetting.enable = true;
     open = false;
     powerManagement.enable = false;
     prime = {
       offload.enable = true;
       offload.enableOffloadCmd = config.hardware.nvidia.prime.offload.enable;
+      sync.enable = false;
       nvidiaBusId = "PCI:01:00:0";
       intelBusId = "PCI:00:02:0";
-      sync.enable = false;
     };
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
@@ -21,6 +20,17 @@
     "modesetting"
     "nvidia"
   ];  
+  services.xserver.deviceSection = ''
+    Driver "nvidia"
+    Option "AllowEmptyInitialConfiguration"
+  '';
+  services.xserver = {
+    enable = true;
+    xkb.layout = vars.keyboardLayout;
+    xkb.model = vars.keyboardModel;
+    xkb.variant = vars.keyboardVariant;
+    displayManager.startx.enable = true;
+  };
   nixpkgs.config.nvidia.acceptLicense = true;
 
   hardware.graphics = {
