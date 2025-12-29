66b058689dbd96161bf76a36fb9cdf8542b2eacc
alainpham <13981031+alainpham@users.noreply.github.com>
2025-12-29 16:55:50 +0100

    mbp generic

--- hw.ombp.nix
+++ hw.ombp.nix
@@ -1,19 +1,25 @@
 { config, lib, pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_5_10;
-
   boot.extraModulePackages = [ ];
   boot.initrd.kernelModules = [ ];
   boot.blacklistedKernelModules = [ ];
-  
-  hardware.nvidia = {
-    modesetting.enable = false;
-    nvidiaSettings = false;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
-  };
-  services.xserver.videoDrivers = [
-    "nvidia"
-  ];
-  nixpkgs.config.nvidia.acceptLicense = true;
-
 }
+# { config, lib, pkgs, ... }:
+# {
+#   boot.kernelPackages = pkgs.linuxKernel.packages.linux_5_10;
+
+#   boot.extraModulePackages = [ ];
+#   boot.initrd.kernelModules = [ ];
+#   boot.blacklistedKernelModules = [ ];
+  
+#   hardware.nvidia = {
+#     modesetting.enable = false;
+#     nvidiaSettings = false;
+#     package = config.boot.kernelPackages.nvidiaPackages.legacy_340;
+#   };
+#   services.xserver.videoDrivers = [
+#     "nvidia"
+#   ];
+#   nixpkgs.config.nvidia.acceptLicense = true;
+
+# }
