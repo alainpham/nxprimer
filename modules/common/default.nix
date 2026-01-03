5d4edb6a299aa8940b77fa09377fffaf43957d7f
alainpham <13981031+alainpham@users.noreply.github.com>
2026-01-03 10:41:55 +0100

    new files

---
+++ modules/common/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # fastboot
+  boot.loader.timeout = 1;
+
+  system.stateVersion = nixStateVersion;
+
+  networking.hostName = vars.hostname;
+  networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "dnsmasq";
+
+  environment.etc."NetworkManager/dnsmasq.d/dev.conf".text = ''
+    #/etc/NetworkManager/dnsmasq.d/dev.conf
+    local=/${vars.wildcardDomain}/
+    address=/${vars.wildcardDomain}/172.18.0.1
+  '';
+  environment.etc."NetworkManager/dnsmasq.d/vms".source = "/home/${vars.targetUserName}/virt/runtime/vms";
+  environment.homeBinInPath = true;
+}
