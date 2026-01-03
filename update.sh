53d1dfd3f7d2a7b1048d5e63a03ac91838aa63c2
alainpham <13981031+alainpham@users.noreply.github.com>
2026-01-03 10:43:01 +0100

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo cp configuration.nix /etc/nixos
 sudo cp vars/${HOSTNAME}.nix /etc/nixos/vars.nix
 sudo cp flake.nix /etc/nixos/flake.nix
-
 sudo cp hw/${HOSTNAME}.nix /etc/nixos/hw.nix
+sudo cp -r modules /etc/nixos/
 
 sudo nixos-rebuild switch --flake /etc/nixos#nixos
