f5eddf4d053f8b9e5416edb6a6cb3d6026770b29
alainpham <13981031+alainpham@users.noreply.github.com>
2026-01-17 00:26:13 +0100

    fujb

--- hw/fujb.nix
+++ hw/fujb.nix
@@ -1,48 +1,13 @@
 { config, lib, pkgs, stdenv... }:
 let
-  decklinkSdk = stdenv.mkDerivation rec {
-    pname = "decklink-sdk";
-    version = "15.3";
+ 
 
-    buildInputs = [
-      autoPatchelfHook
-      libcxx
-      libcxxabi
-      libGL
-      gcc
-      unzip
-    ];
-
-    # yes, the below download function is an absolute mess.
-    # blame blackmagicdesign.
-    src = runCommandLocal "${pname}-${lib.versions.majorMinor version}-src.zip"
-      rec {
-
-        impureEnvVars = lib.fetchers.proxyImpureEnvVars;
-        nativeBuildInputs = [ curl unzip ];
-      } ''
-      curl -L -o $out/sdk.zip "http://192.168.8.100:28000/blackmagic/Blackmagic_DeckLink_SDK_15.3.zip"
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      echo $NIX_BUILD_TOP
-
-      mkdir -p $out/include
-      cp -r $NIX_BUILD_TOP/Blackmagic\ DeckLink\ SDK\ 12.5/Linux/include $out
-
-      runHook postInstall
-    '';
-  }
-
-
-  decklinkFfmpeg = pkgs.ffmpeg.overrideAttrs (oldAttrs: {
+  decklinkffmpeg = pkgs.ffmpeg.overrideAttrs (oldAttrs: {
     configureFlags = oldAttrs.configureFlags ++ [ "--enable-nonfree" "--enable-decklink" ];
     nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
     buildInputs = oldAttrs.buildInputs ++ [
       pkgs.blackmagic-desktop-video
-      decklinkSdk
+      decklinksdk
     ];
     
     postFixup = ''
@@ -72,8 +37,8 @@ in
 
   environment.systemPackages = with pkgs; [
     blackmagic-desktop-video # for blackmagic capture card
-    decklinkSdk
-    decklinkFfmpeg 
+    decklinksdk
+    decklinkffmpeg 
   ];
 
 
