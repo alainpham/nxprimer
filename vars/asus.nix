f6db127b4b4e5df316914207968c340944df925b
alainpham <13981031+alainpham@users.noreply.github.com>
2026-01-20 22:06:31 +0100

    asus

---
+++ vars/asus.nix
@@ -0,0 +1,22 @@
+{
+  hostname= "asus";
+  targetUserName = "apham";
+  targetUserEmail = "apham@localhost";
+  keyboardLayout = "fr";
+  keyboardModel = "pc105";
+  keyboardVariant = "mac";
+  wildcardDomain = "houze.dns.army";
+  disableTurboBoost = true;
+  numlockOnBoot = false;
+  automaticlogin = true;
+
+  enableDev = true;
+  enableContainers = true;
+  enableVirtualization = true;
+  enableGui = true;
+  enableWorkstation = true;
+  enableGaming = true;
+  
+  enablePicom = true;
+  sunshineOnBoot = false;
+}
