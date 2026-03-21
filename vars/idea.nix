a6fd8548238b4005be95f09d5d21ea3879a8ffe7
alainpham <13981031+alainpham@users.noreply.github.com>
2026-03-22 00:37:56 +0100

    idea

---
+++ vars/idea.nix
@@ -0,0 +1,24 @@
+{
+  hostname= "asus";
+  targetUserName = "apham";
+  targetUserEmail = "apham@localhost";
+  keyboardLayout = "fr";
+  keyboardModel = "pc105";
+  keyboardVariant = "azerty";
+  wildcardDomain = "houze.dns.army";
+  disableTurboBoost = false;
+  numlockOnBoot = true;
+  automaticlogin = true;
+
+  enableDev = true;
+  enableContainers = true;
+  enableVirtualization = true;
+  enableDwm = true;
+  enableStartx = true;
+  enablePicom = true;
+  enableGui = true;
+  enableWorkstation = true;
+  enableGaming = true;
+  
+  sunshineOnBoot = true;
+}
