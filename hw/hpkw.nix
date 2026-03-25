7bb43f77b963aeec1fccb8e3f68cf1482f8e5199
alainpham <13981031+alainpham@users.noreply.github.com>
2026-03-25 17:29:52 +0100

    hpwkw

---
+++ hw/hpkw.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+{
+  boot.extraModulePackages = [ ];
+  boot.initrd.kernelModules = [ ];
+  boot.blacklistedKernelModules = [ ];
+  
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    powerManagement.enable = true;
+    prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = config.hardware.nvidia.prime.offload.enable;
+      sync.enable = false;
+      intelBusId = "PCI:00:02:0";
+      nvidiaBusId = "PCI:01:00:0";
+    };
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  services.xserver.videoDrivers = [
+    "nvidia"
+  ];
+
+  nixpkgs.config.nvidia.acceptLicense = true;
+
+  hardware.graphics = {
+    extraPackages = with pkgs; [
+      intel-media-driver
+    ];
+  };
+
+  programs.obs-studio.package = pkgs.obs-studio.override {
+    cudaSupport = true;
+  };
+}
---
+++ vars/hpkw.nix
@@ -0,0 +1,24 @@
+{
+  hostname= "hpkw";
+  targetUserName = "apham";
+  targetUserEmail = "apham@localhost";
+  keyboardLayout = "de";
+  keyboardModel = "pc105";
+  keyboardVariant = "";
+  wildcardDomain = "houze.dns.army";
+  disableTurboBoost = true;
+  numlockOnBoot = true;
+  automaticlogin = true;
+
+  enableDev = true;
+  enableContainers = true;
+  enableVirtualization = true;
+  enableDwm = true;
+  enableStartx = true;
+  enablePicom = true;
+  enableGui = true;
+  enableWorkstation = true;
+  enableGaming = true;
+  
+  sunshineOnBoot = true;
+}
