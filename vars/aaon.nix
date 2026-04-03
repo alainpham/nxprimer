5f2e1aa3882f8bef6c0913cd3be7fcd26f03333b
alainpham <13981031+alainpham@users.noreply.github.com>
2026-04-04 00:52:15 +0200

    aaon

---
+++ vars/aaon.nix
@@ -0,0 +1,24 @@
+{
+  hostname= "aaon";
+  targetUserName = "user";
+  targetUserEmail = "user@localhost";
+  keyboardLayout = "fr";
+  keyboardModel = "pc105";
+  keyboardVariant = "azerty";
+  wildcardDomain = "paaon.dns.army";
+  disableTurboBoost = true;
+  numlockOnBoot = true;
+  automaticlogin = true;
+
+  enableDev = true;
+  enableContainers = true;
+  enableVirtualization = true;
+  enableDwm = true;
+  enableStartx = false;
+  enablePicom = true;
+  enableGui = true;
+  enableWorkstation = true;
+  enableGaming = true;
+  
+  sunshineOnBoot = true;
+}
