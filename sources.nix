8827abdd427dbfacc81b5e4ff058bb9fa9258439
alainpham <13981031+alainpham@users.noreply.github.com>
2026-05-02 13:34:18 +0200

    xdpi

--- sources.nix
+++ sources.nix
@@ -7,7 +7,7 @@ in
   dotfilesgit = builtins.fetchGit {
     url = "https://github.com/alainpham/dotfiles.git";
     ref = "master";
-    rev = "7bfe601a954ab89766822bba3cd4e97cb590d6b1";
+    rev = "c084bb3c44c096279110e09f16637aaf2f6e3683";
   };
 
   # desktop related
