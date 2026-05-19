d7b50c02d773a91769731e613eb7b2e2dce5a30e
alainpham <13981031+alainpham@users.noreply.github.com>
2026-05-19 14:05:58 +0200

    audio

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -71,6 +71,19 @@
     jack.enable = true;
   };
 
+  security.pam.loginLimits = [
+    { domain = "@audio"; item = "memlock"; type = "-"   ; value = "unlimited"; }
+    { domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
+    { domain = "@audio"; item = "nofile" ; type = "soft"; value = "99999"    ; }
+    { domain = "@audio"; item = "nofile" ; type = "hard"; value = "99999"    ; }
+  ];
+  
+  services.udev.extraRules = lib.mkAfter 
+    ''
+      KERNEL=="rtc0", GROUP="audio"
+      KERNEL=="hpet", GROUP="audio"
+    '';
+    
   services.pulseaudio = {
     enable = false;
     support32Bit = true;
